661890a479b09e1b1a5337cf4fd5fa4e56747975
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-20 11:01:21 +0500

    initial commit and done cleanup

---
+++ src/features/app/appSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {}
+
+export const counterSlice = createSlice({
+  name: 'app',
+  initialState,
+
+  reducers: {},
+})
+
+export default counterSlice.reducer
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyANDCru94ID3X0u7cUf9jRq9A9jAsSCdHc',
+  authDomain: 'snapchat-clone-5841f.firebaseapp.com',
+  projectId: 'snapchat-clone-5841f',
+  storageBucket: 'snapchat-clone-5841f.appspot.com',
+  messagingSenderId: '879234296984',
+  appId: '1:879234296984:web:e9a68648d0499e22f2b4ed',
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
